67928ca2dd70b91a5064f98c05ad1d36ecd59263
Evan <39568306+Evanlyboy@users.noreply.github.com>
2022-11-08 12:50:26 -0500

    added vue component

---
+++ js/components/TheHeroThumbnail.js
@@ -0,0 +1,13 @@
+export default {
+    name: "TheHeroThumb",
+
+    props: {
+        hero: Object
+    },
+
+    template: `
+        <section>
+            <h1>Hero Component</h1>
+        </section>
+    `
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
+// import components first
+import ImportHeroThumbnail from './components/TheHeroThumbnail.js';
+
 (() => {
     // create vue instance here
     const { createApp } = Vue
 
     createApp({
         created() {
-            
+            // always get your remote data here
+            fetch('./data.json')
+                .then(res => res.json())
+                .then(data => this.heroData = data)
+                .catch(error => console.error(error));
         },
 
         data() {
             return {
                 heroData: {}
             }
+        },
+
+        components: {
+            // must be lower case
+            herothumbnail: ImportHeroThumbnail
         }
     }).mount('#app')
 })()
